ce127d6a09056b5f4f8b581d8220d65a94aed6e2
Henrique Arnaud Santana Silva <37378502+HenriqueFpg@users.noreply.github.com>
2020-05-01 21:44:00 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res)=>{
     res.status(200).send({message: 'Hello World'});
 });
 
+app.get('/ola', (req, res)=>{
+    res.status(200).send({message: 'Essa é a rota ola', id: 69});
+});
+
 app.listen(3001, () => {
     console.log('Api rodando na porta 3001');
 });
